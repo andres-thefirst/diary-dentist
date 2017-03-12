fefcf569c027075ec595b6f7691faf51416c33c8
Andres TheFirst <darkshadowskull@gmail.com>
2017-03-11 19:25:26 -0600

    Creates AddAppointment Component

---
+++ src/AddAppointment.js
@@ -0,0 +1,57 @@
+import React, {Component} from 'react';
+import './AddAppointment.css';
+
+class AddAppointment extends Component{
+
+  constructor(props){
+    super(props);
+    this.kindConsultationList = props.kindConsultationList.map((option) => <option value={option.id} key={option.name}>{option.name}</option>);
+
+    let timeList = [...this.generateTimeArray(7,22)];
+
+    this.timeList = timeList.map((option) => <option value={option.id} key={option.name}>{option.name}</option>);
+    this.state = { kindConsultation: props.kindConsultationList[0].id, time: timeList[0].id};
+  }
+
+  generateTimeArray = (from, to) => {
+    let timeArray = []
+    for(let i = from; i <= to ; i++) {
+      timeArray.push({id: i, name: `${i}:00`});
+    }
+    return timeArray;
+  }
+
+  handleChange = (event) => {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+    this.setState({
+      [name]: value
+    });
+  }
+
+  onSubmit = (event) => {
+    event.preventDefault();
+    console.log('sending data...', `${this.state.kindConsultation} ${this.state.time}`);
+  }
+
+  render = () => (
+      <div>
+        <form onSubmit={this.onSubmit}>
+          <label>Tipo de Consulta:
+            <select name='kindConsultation' onChange={this.handleChange}>
+              {this.kindConsultationList}
+            </select>
+          </label>
+          <label>Horario:
+            <select name='time' onChange={this.handleChange}>
+              {this.timeList}
+            </select>
+          </label>
+          <input type='submit' value='Aceptar' />
+        </form>
+      </div>
+  );
+}
+
+export default AddAppointment;
--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import AddAppointment from './AddAppointment';
 import './App.css';
 
 class App extends Component {
+
   render() {
+
+    const kindConsultationList= ['Brackets', 'Odontologia', 'Sacada de Muela', 'Limpieza bucal'].map((item, index) => ({ id:index, name:item }));
+
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <AddAppointment kindConsultationList={kindConsultationList}/>
       </div>
     );
   }
